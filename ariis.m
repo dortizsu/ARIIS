17b5db70552c1dd9ff055e1f86162611aaafe355
Dave Ortiz-Suslow <dortizsu@nps.edu>
2019-07-30 21:39:18 -0700

    Cleaned up sub-routines, namely modernized spectf

--- ariis.m
+++ ariis.m
@@ -372,11 +372,8 @@ end
 %...APPENDIX......................................................................
 %.................................................................................
 function ust = ustar(u,v,w)
-    % get friction velocity from 3 velocity components
-	%
-	% author: D.G. Ortiz-Suslow
-	% revised: 01/31/19
-	% disclaimer: This software is provided as-is with no guarantee of functionality or fitness for the task for which it was designed to complete.
+    % Determine friction velocity from three orthogonal components
+	% contributor: DOS
 	up = u - mean(u);
 	vp = v - mean(v);
 	wp = w - mean(w);
@@ -389,14 +386,11 @@ function ust = ustar(u,v,w)
 end
 %
 function rated = rate(x,flag)
-	% rated = rate(x,flag)
-	% flag = 1 (output rate vector), flag = 2 (output mean rate), flag = 3 (output [mean std])
-	% calculate forward difference of a column vector. Based on flag output is a column vector of same dimension as input, the mean difference, or the mean and stan. dev.
-	%
-	% author: D.G. Ortiz-Suslow
-	% revised: 01/31/19
-	% disclaimer: This software is provided as-is with no guarantee of functionality or fitness for the task for which it was designed to complete.
-	%
+	% do 1st order difference
+	% flag = 1 output vector of differences same size as input (if no flag given also do this)
+	% flag = 2 output mean difference
+	% flag = 3 output [mean std]
+	% contributor: DOS
 	if ~iscolumn(x)
 	    x = x';
 	end
@@ -412,18 +406,11 @@ function rated = rate(x,flag)
 end
 %
 function [rfitted,raw] = rlogfit(x,y,varargin)
-	% [rfitted,raw] = rlogfit(x,y)
-	% [rfitted,raw] = rlogfit(x,y,Dthresh)
-	% [rfitted,raw] = rlogfit(x,y,fittype,Dthresh)
-	%
 	% Iterative, robust linear least-squares regression.
 	% Method uses a Cook's Distance algorithm to flag/remove suspicious and influential points from final regression model.
-	% function is based on logfit.m (Matlab fileexchange)
-	%
-	% author: D.G. Ortiz-Suslow
-	% revised: 01/31/19
-	% disclaimer: This software is provided as-is with no guarantee of functionality or fitness for the task for which it was designed to complete.
-	%
+	% function is based on logfit.m (Matlab fileexchange) included below.
+	% contributor: DOS
+	
 	% Initial regression model fitting
 	if nargin < 4
 		[fittype,M,B,MSE,R2,S] = logfit(x,y,'nograph');
@@ -561,34 +548,31 @@ end
 function sep = stderr(x,y,yf)
 	% sep = stderr(x,y,yf)
 	% Calculate slope/intercept standard errors for linear regression
+	% Contributor: DOS, source MATLAB documentation for fitting toolbox
 	N = length(x);
-	if N ~= length(y) || N ~= length(yf)
-		error('Inconsistent dimensions of inputs.')
-	end
-	R = y-yf;
+	R = y-yf; % residual
 	SXX = sum(var(x));
-	MSE = sum(R.^2)/(N-2);
+	MSE = sum(R.^2)/(N-2); % mean squared "error" or residual
 	sep = [...
-	sqrt(MSE*sum(x.^2)/(N*SXX)),...
-	sqrt(MSE/SXX)];
+	sqrt(MSE*sum(x.^2)/(N*SXX)),... % error for slope
+	sqrt(MSE/SXX)]; % error for intercept
 end
 %
 function [sf,stdsf]=logSmooth(f,varargin)
 	%
-	%  [sf,stdsf]=logSmooth(f:array,a)
+	% [sf,stdsf]=logSmooth(f,a)
+	% [sf,stdsf]=logSmooth(f)
 	%
-	% Log-uniform smoothing of f using bin-averaging technique.
+	% Log-uniform smoothing of f using a bin-averaging technique.
+	%
 	% Inputs:
 	% f --> column vector needing smoothing. If f is array, operates on columns.
-	% !! logSmooth assumes column vector or that inputs are to be operated over columns !!
-	% a (optional) --> control width of smoothing windows, increasing a decreases smooth (default = 4)
+	% a (optional) --> control width of smoothing windows, increasing "a" decreases smoothness (default = 4)
 	%
 	% Outputs:
-	% sf --> smoothed f (or array).
+	% sf --> smoothed array.
 	% stdsf --> standard error of the means over each bin, x1.96 gives 95% confidence interval.
-	%
-	% Original code: Ioannis Kalogiros (5/3/2000) in Matlab v5.3
-	% Including "a": D. Ortiz-Suslow (2018) Matlab v2018a/
+	% Contributor: John Kalogiros (2000) & DOS
 	%
 	if size(f,1) == 1
 		f = f(:);
@@ -707,184 +691,132 @@ function out = sem(varargin)
 end	
 %
 %.................................................................................
-% below is an unaltered copy/paste of spectf.m
 % DISCLAIMER: DOS did NOT write this, original code from M. Donelan & W. Drennan circa 1995
 %.................................................................................
 %
-function S = spectf(x,y,dt,Nfa,a0)                              
+function S = spectf(varargin)
+	% S = SPECTf(x,dt)
 	% S = SPECTf(x,dt,Nfa)
-	% S = SPECTf(x,y,dt,Nfa)   cross spectrum
-	% 
-	% Frequency averaged power spectrum estimate,  GEOPHYSICAL NORMALIZATION
-	% Trend is removed, Blackman-Harris window is used. K.K.Kahma 1990-05-19
-	%
-	% x , y  = data vectors
-	% dt = sampling interval in seconds
-	% Nfa = number of elementary frequency bands which are averaged
-	%
-	% S(:,1) = f      (1/second == Hz)
-	% S(:,2) = Sxx    (unit*unit*second)
-	%
-	% If cross spectrum is calculated 
-	% S(:,3) = Syy
-	% S(:,4) = Sxy
-	% S(:,5) = phase angle = 180/pi*atan2(-imag(Sxy),real(Sxy))
-	% S(:,6) = coherence   = abs(Sxy./sqrt(Sxx.*Syy))
-	%
-	% positive phase means x leads y
-
+	% S = SPECTf(x,y,dt)
+	% S = SPECTf(x,y,dt,Nfa)
 	% S = SPECTF(x,y,dt,Nfa,a0)
-	% Elementary frequency bands 0:a0-1 (matlab index 1:a0) are ignored. 
-	% Default a0 is 0, i.e. all bands including zero (mean value) are included.
-	% 
-
-	x = x(:).';      	% Make sure x is a row vector
-	N  = max(size(x));      % Number of data points
-	window=blackhar(N)';
+	% calculate power spectral density and cross-sepctrum if needed
+	% contributors: M. Donelan, W. Drennan, with modifications/cleaning from DOS
+	Nfa = 31;
+	a0 = 0;
+	Nargsin = numel(varargin);
+	x = varargin{1}; x = x(:)';
+	N  = numel(x);
+	window = blackhar(N)'; % window function
 	% window=hamming(N)';
-
-	if max(size(y)) ~= N
-	   if (max(size(y)) == 1) || (nargin < 5)
-
-	% ***************
-	   % Spectrum 
-	% ***************
-
-	   if (nargin < 4), Nfa = 0; end    % default a0
-	   if (nargin < 3), dt = 31; end    % default Nfa
-	   a0 = Nfa; Nfa = dt; dt = y; 
-
-	   Nfft=0;
-	   maxb=0;
-	   C=0; 
-	   df=0;  % To define these variables before Xx'
-	   Xx = fft(window.*detrend(x));
-	   Nfft = length(Xx);                 % Number of points in FFT
-	   maxb = floor(Nfft/2)+1;
-	   Xx(maxb+1:Nfft)=[];
-	   Xx(maxb) = Xx(maxb)/2;
-
-	   C = dt/(Nfa*pi*norm(window)^2);    % Scaling coefficient
-	   df = 2*pi/(dt*Nfft);
-  
-	   if Nfa==1
-	      f = [a0:maxb-1]*df;
-	      Pxx = (abs(Xx(a0+1:maxb)).^2)*C;
-	   else
-	      if Nfa > 20
-	%       When Nfa is large enough this is as fast as vectorized
-	%       averaging and it requires far less memory    
-	        m=0; a=a0+1; b=a0+Nfa;
+	%
+	% figure out what you gave us...
+	if Nargsin == 2
+		dt = varargin{2};
+	elseif Nargsin == 3
+		if numel(varargin{2}) == 1
+			dt = varargin{2};
+			Nfa = varargin{3};
+		else
+			y = varargin{2};
+			dt = varargin{3};
+		end
+	elseif Nargsin > 3
+		y = varargin{2}; y = y(:)';
+		dt = varargin{3};
+		Nfa = varargin{4};
+		if Nargsin > 4
+			a0 = varargin{5};
+		end
+	end
+	%
+	% proceed to spectral calculations
+	%
+    Xx = fft(window.*detrend(x));
+	% number of points in FFT
+    Nfft = length(Xx);
+    maxb = floor(Nfft/2)+1;
+    Xx(maxb+1:Nfft) = [];
+    Xx(maxb) = Xx(maxb)/2;
+    C = dt/(Nfa*pi*norm(window)^2);    % Scaling coefficient
+    df = 2*pi/(dt*Nfft);
+	%
+	if Nfa == 1
+		f = [a0:maxb-1]*df;
+		Pxx = (abs(Xx(a0+1:maxb)).^2)*C;
+	else
+		if Nfa > 20 
+			% When Nfa is large enough this is as fast as vectorized
+			% averaging and it requires far less memory    
+	        m = 0;
+			a = a0+1;
+			b = a0+Nfa;
 	        while b <= maxb
-	           m=m+1;
-	           Pxx(m) = sum(abs(Xx(a:b)).^2)*C;
-	           f(m) = df*((a+b-2)/2);
-	           a=a+Nfa; b=b+Nfa;
-	        end
-	      else
-	        m=fix((maxb-a0) / Nfa);
-	        f=([1:m]*Nfa+(a0-0.5-Nfa/2))*df;
-	        b=a0+m*Nfa;
-
-	%        Old bin averaging loop
-	%        sx=zeros(m,Nfa);
-	%        for i=1:Nfa 
-	%           sx(:,i) = abs(Xx(a0+i:Nfa:b)).^2;
-	%        end
-	%        Pxx=(sum(sx.')*C);
-
-	        sx=zeros(Nfa,m);
+				m = m+1;
+				Pxx(m) = sum(abs(Xx(a:b)).^2)*C;
+				f(m) = df*((a+b-2)/2);
+				a = a + Nfa;
+				b = b + Nfa;
+			end
+		else
+			m = fix((maxb - a0)/Nfa);
+	        f = ((1:m)*Nfa + (a0 - 0.5 - Nfa/2))*df;
+	        b = a0 + m*Nfa;
+	        sx = zeros(Nfa,m);
 	        sx(:) = abs(Xx(a0+1:b)).^2;  
 	        Pxx=sum(sx)*C;
-	      end
-	      a=a0+1+m*Nfa;
-	      if a <= maxb
-	         m=m+1;
-	         c = maxb+1-a; 
-	         Pxx(m) = sum(abs(Xx(a:maxb)).^2)*C*Nfa/c;
-	         f(m) = df*(a+maxb-2)/2;
-	      end
-	    end 
-	    clear Xx window
-	    S = [f/2/pi;2*pi*Pxx].';
- 
-	   else
-
-	  error('x and y are not of same size'); end
-
-	else
-
-	% **********************
-	   % Cross spectrum
-	% **********************
-
-	   if (nargin < 5), a0 = 0; end    % default a0
-	   if (nargin < 4), Nfa = 31; end  % default Nfa
-	   y = y(:).';
-	   Nfft=0; maxb=0; C=0; df=0;
-	   Xx = fft(window.*detrend(x));
-	   Nfft = length(Xx);                 % Number of points in FFT
-	   maxb = floor(Nfft/2)+1;
-	   Xx(maxb+1:Nfft)=[];
-	   Xx(maxb) = Xx(maxb)/2;
-
-	   C = dt/(Nfa*pi*norm(window)^2);    % Scaling coefficient
-	   df = 2*pi/(dt*Nfft);
-
-	   Yy = fft(window.*detrend(y));
-	   Yy(maxb) = Yy(maxb)/2;
-	   Yy(maxb+1:Nfft)=[];
-
+		end
+        a = a0+1+m*Nfa;
+        if a <= maxb
+           m = m+1;
+           c = maxb+1-a; 
+           Pxx(m) = sum(abs(Xx(a:maxb)).^2)*C*Nfa/c;
+           f(m) = df*(a+maxb-2)/2;
+        end
+	    S = [f/2/pi;2*pi*Pxx]';
+	end
+	%
+	% cross spectrum if necessary
+	if exist('y','var')
+ 	   Yy = fft(window.*detrend(y));
+ 	   Yy(maxb) = Yy(maxb)/2;
+ 	   Yy(maxb+1:Nfft) = [];
 	   if Nfa==1
-	      f = [a0:maxb-1]*df;
-	      Pxx = (abs(Xx(a0+1:maxb)).^2)*C;
-	      Pyy = (abs(Yy(a0+1:maxb)).^2)*C;
-	      Pxy = (conj(Xx(a0+1:maxb)).*Yy(a0+1:maxb))*C;
+		   Pyy = (abs(Yy(a0+1:maxb)).^2)*C;
+		   Pxy = (conj(Xx(a0+1:maxb)).*Yy(a0+1:maxb))*C;
 	   else
-	      if Nfa > 20
-	         m=0; a=a0+1; b=a0+Nfa;
-	         while b <= maxb
-	            m=m+1;
-	            Pxx(m) = sum(abs(Xx(a:b)).^2)*C;
-	            Pyy(m) = sum(abs(Yy(a:b)).^2)*C;
-	            Pxy(m) = sum(conj(Xx(a:b)).*Yy(a:b))*C;
-	            f(m) = df*((a+b-2)/2);
-	            a=a+Nfa; b=b+Nfa;
-	         end
-	      else
-	         m=fix((maxb-a0) / Nfa);
-	         f=([1:m]*Nfa+(a0-0.5-Nfa/2))*df;
-	         b=a0+m*Nfa;
-	%         sx=zeros(m,Nfa);
-	%         for i=1:Nfa 
-	%            sx(:,i)  = abs(Xx(a0+i:Nfa:b)).^2;
-	%            sy(:,i)  = abs(Yy(a0+i:Nfa:b)).^2;
-	%            sxy(:,i) = conj(Xx(a0+i:Nfa:b)).*Yy(a0+i:Nfa:b);
-	%         end
-	         sx=zeros(Nfa,m);
-	         sx(:) = abs(Xx(a0+1:b)).^2;  
-	         Pxx=(sum(sx)*C);
-	         sx(:) = abs(Yy(a0+1:b)).^2;  
-	         Pyy=(sum(sx)*C);
-	         sx(:) = conj(Xx(a0+1:b)).*Yy(a0+1:b);  
-	         Pxy=(sum(sx)*C);
-	         a=a0+1+m*Nfa;
-	      end
-   
-	      if a <= maxb
-	         m=m+1; 
-	         c = maxb+1-a; 
-	         Pxx(m) = sum(abs(Xx(a:maxb)).^2)*C*Nfa/c;
-	         Pyy(m) = sum(abs(Yy(a:maxb)).^2)*C*Nfa/c;
-	         Pxy(m) = sum(conj(Xx(a:maxb)).*Yy(a:maxb))*C*Nfa/c;
-	         f(m) = df*(a+maxb-2)/2;
-	      end
-	   end
-	   phase = atan2d(-imag(Pxy),real(Pxy));
-	   coh   = abs(Pxy./sqrt(Pxx.*Pyy));
-	   clear Xx Yy window sx sy sxy
-	   S = [f/2/pi;2*pi*Pxx;2*pi*Pyy;2*pi*Pxy;phase;coh].';
-   end
+		   if Nfa > 20
+			   m=0;
+			   a=a0+1;
+			   b=a0+Nfa;
+			   while b <= maxb
+				   m = m+1;
+				   Pyy(m) = sum(abs(Yy(a:b)).^2)*C;
+				   Pxy(m) = sum(conj(Xx(a:b)).*Yy(a:b))*C;
+				   a = a + Nfa;
+				   b = b + Nfa;
+			   end
+		   else
+			   m = fix((maxb - a0)/Nfa);
+			   b = a0 + m*Nfa;
+			   sx = zeros(Nfa,m);
+			   sx(:) = abs(Yy(a0+1:b)).^2;  
+			   Pyy=(sum(sx)*C);
+			   sx(:) = conj(Xx(a0+1:b)).*Yy(a0+1:b);  
+			   Pxy=(sum(sx)*C);
+			   a = a0 + 1 + m*Nfa;
+		  end
+		  if a <= maxb
+		     m = m+1; 
+		     c = maxb+1-a; 
+		     Pyy(m) = sum(abs(Yy(a:maxb)).^2)*C*Nfa/c;
+		     Pxy(m) = sum(conj(Xx(a:maxb)).*Yy(a:maxb))*C*Nfa/c;
+		  end
+		end
+ 	    phase = atan2d(-imag(Pxy),real(Pxy));
+ 	    coh = abs(Pxy./sqrt(Pxx.*Pyy));
+ 	    S = [S (2*pi*Pyy)' (2*pi*Pxy)' phase' coh'];
+	end
 end
 %
 %.................................................................................
@@ -894,12 +826,12 @@ end
 %
 function w = blackhar(n)
     %
-    %  BLACKHAR(N) returns the N-point Blackman-Harris window as a column vector
-    %  K.Kahma 1989-07-20
-
-    m = (0:n-1)' * ( 2*pi/(n-1) ) ;
-
-    w = (.35875 - .48829*cos(m) + .14128*cos(2*m) - 0.01168*cos(3*m) ) ;
+    % blackhar(N) 
+	% returns the N-point Blackman-Harris window as a column vector
+    % contributor: K.Kahma 1989-07-20
+    m = (2*pi/(n-1))*(0:n-1);
+	m = m';
+    w = (0.35875 - 0.48829*cos(m) + 0.14128*cos(2*m) - 0.01168*cos(3*m));
 end
 %.................................................................................
 % below is an unaltered copy/paste of logfit.m
